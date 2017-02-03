9fa905c2bd1756ab9661d0047d04e75c1f125ef5
MGee123 <mattgordon@hotmail.co.uk>
2017-02-03 09:43:10 +0000

    exercises

---
+++ exercise.js
@@ -0,0 +1,23 @@
+var firstName = "Matthew";
+var lastName = "Gordon";
+console.log(firstName + " " + lastName);
+
+
+function firstNameLastName(firstName, lastName) {
+return firstName + " " + lastName;
+};
+firstNameLastName("Matthew", "Gordon");
+
+
+function lastNameFirstName(firstName, lastName) {
+return lastName + ", " + firstName;
+};
+lastNameFirstName("Matthew", "Gordon");
+
+var firstName = "Matthew"
+alert(firstName.length);
+
+
+
+
+
