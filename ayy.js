ac2e4529485b3d98dbbe6a1541ad76406439ffbb
MGee123 <mattgordon@hotmail.co.uk>
2017-02-08 18:30:16 +0000

    adding comments

--- ayy.js
+++ ayy.js
@@ -1,42 +1,42 @@
 var gillFamily = [{name: 'john', age: 20}, {name: 'richard', age: 27}, {name: 'debbie', age: 55}, {name: 'dan', age: 25}, {name: 'robin', age: 60}];
 
-1.
+//1.
 _.map(gillFamily, "name");
 
-2.
+//2.
 
 
 _.map(gillFamily, (el) => el.name + " Gill");
 
-3.
+//3.
 
 _.sumBy(gillFamily, function(o) { return o.age; }); 
 _.sumBy(gillFamily, "age");
 
-4.
+//4.
 _(gillFamily).filter("age").reduce(function(a,m,i,p) {
 return a + m.age/p.length;
 },0);
 
-5.
+//5.
 _.map(_.filter(gillFamily, function (person) { return person.age < 50; }),
   function (person) { return person.name; });
 
-6.
+//6.
 _.countBy(gillFamily, name);
 
-7.
+//7.
 _.filter(gillFamily, function (person) {
   return person.age < 50;
 }).length
-8.
+//8.
 _.chain(gillFamily)
   .filter(function (person) { return person.age < 50; })
   .map(function (person) { return person.name + ' Gill'; })
   .join(', ')
   .value()
 
-9.
+//9.
 <table>
 <tr>
 <th>Name</th>
@@ -49,41 +49,42 @@ _.chain(gillFamily)
 <td> _.map(gillFamily, "age"); </td>>
 </tr>
 </table>
-10.
+
+//10.
 _.map(gillFamily, function(over) {
     return over.age > 26 ? _.omit(over, 'age') : over;
 });
 
-11.
+//11.
 _.sortBy(gillFamily, "age");
 
-12.
+//12.
 _.filter(gillFamily, function(dee) {
 return dee.name.charAt(0) === "d";
 }); 
 
 
-13.
+//13.
 _.groupBy(gillFamily, function(firstLetter) {
 return firstLetter.name.charAt(0) ; 
 });
 
-14.
+//14.
 _.min(gillFamily, "age"); 
 
 
-15.
+//15.
 
 _.filter(gillFamily, function(aIn) {
 return aIn.name.includes("a");
 });
 
-16.
+//16.
 _.map(gillFamily, function(person) {
 return person.name.charAt(0).toUpperCase() + person.name.slice(1);
 });
 
-17.
+//17.
 .filter(_.min(gillFamily, "age", function(personY) {
 return function(aIn) {
 return aIn.name.includes("a");
